232dc3ed8139b593299c3f3fb5a4846749eed16a
Kostas Tzoumpas <kststz@gmail.com>
2024-05-24 14:50:34 +0300

    update wasm to 0.50.1

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,13 +5,12 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           35
+// Upgrade:                              1
+// Endpoints:                           34
 // Async Callback (empty):               1
 // Total number of exported functions:  37
 
 #![no_std]
-#![allow(internal_features)]
-#![feature(lang_items)]
 
 multiversx_sc_wasm_adapter::allocator!();
 multiversx_sc_wasm_adapter::panic_handler!();
