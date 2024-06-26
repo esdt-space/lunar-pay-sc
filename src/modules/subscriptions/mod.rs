214b47fd21ae3c35b21ba99be100494220042e69
Kostas Tzoumpas <kststz@gmail.com>
2024-06-26 21:54:40 +0300

    cargo fmt

--- src/modules/subscriptions/mod.rs
+++ src/modules/subscriptions/mod.rs
@@ -1,11 +1,10 @@
 pub mod amount;
 pub mod cycles;
 pub mod events;
+pub mod member_endpoints;
+pub mod owner_endpoints;
+pub mod public_endpoints;
 pub mod storage;
 pub mod types;
 pub mod validation;
 pub mod views;
-
-pub mod member_endpoints;
-pub mod owner_endpoints;
-pub mod public_endpoints;
